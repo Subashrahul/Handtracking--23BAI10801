2db00dfb287436797d6f820d10f3994088b37d17
Subashrahul <subashaspirant7876@gmail.com>
2026-03-26 11:00:27 +0530

    Add files via upload

---
+++ handtracking.py
@@ -0,0 +1,17 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+cap.set(3,1280)
+cap.set(4,720)
+detector = HandDetector(detectionCon=0.8)
+
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmlist, _ = detector.findPosition(img)
+    cv2.rectangle(img, (100,100), (300,300), (255, 0 ,255), cv2.FILLED)
+    
+    cv2.imshow("Image",img)
+    
